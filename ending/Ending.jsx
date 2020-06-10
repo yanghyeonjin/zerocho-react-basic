5553f0f1a64c6c13ded14c923978e3ef48901c1d
yanghyeonjin <guswls7081@gmail.com>
2020-06-10 11:46:36 +0900

    2-5. 웹팩으로 빌드하기 - webpack 실행하기 (실행 후 dist/app.js 생성됨.) - 1. package.json scripts에 작성 - => "dev": "webpack" - => npm run dev
    - 2. npx webpack
    
    - @babel/core: 기본적인 babel, 최신문법 가능하게 바꿔주는 것
    - @babel/preset-env: 환경에 맞게 알아서 바꿔주는 것
    - @babel/preset-react: jsx 바꿔주는 것
    - babel/loader: babel이랑 webpack 연결해주는 것

--- ending/Ending.jsx
+++ ending/Ending.jsx
@@ -3,8 +3,14 @@ const React = require('react');
 const { Component } = React;
 
 class Ending extends Component {
-    render() {
+    state = {
+        text: 'Hello, webpack'
+    };
 
+    render() {
+        return (
+            <h1>{this.state.text}</h1>
+        )
     }
 }
 
--- ending/webpack.config.js
+++ ending/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
 
     // * 중요 *
     // entry: 입력 설정, output: 출력 설정
+    // 입력을 받아서 모듈을 적용한 후 (module) 출력한다.
     // client.jsx와 Ending.jsx를 dist/app.js로 합쳐야 한다.
     // 
     // client.jsx가 Ending.jsx를 불러와서 사용하기 때문에 따로 적어줄 필요는 없다. (웹팩이 알아서 뚝딱뚝딱)
@@ -18,6 +19,18 @@ module.exports = {
     entry: {
         app: ['./client']
     },
+
+    module: {
+        rules: [{
+            test: /\.jsx?/, // js와 jsx 파일에 rule을 적용하겠다.
+            loader: 'babel-loader', // 무엇을? babel을 (옛날 문법도 돌아가게끔 해주겠다.)
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties']
+            }
+        }]
+    },
+
     output: {
         path: path.join(__dirname, 'dist'), // 현재 폴더(...../ending) 안에 들어있는 dist --> ...../ending/dist
         filename: 'app.js'
