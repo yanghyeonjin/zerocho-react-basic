055373103478071867c56ec355cc4a4c857eb315
yanghyeonjin <guswls7081@gmail.com>
2020-06-16 09:43:15 +0900

    9-2. Link와 브라우저라우터

--- lecture/ReactRouter.jsx
+++ lecture/ReactRouter.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-import NumberBaseball from '../number-baseball/NumberBaseball';
-import RockPaperScissorsHooks from './RockPaperScissorsHooks';
-import LottoHooks from './LottoHooks';
+import NumberBaseball from '../number-baseball/NumberBaseballClass';
+import RockPaperScissors from './RockPaperScissors';
+import Lotto from './Lotto';
 
 const ReactRouter = () => {
     return (
         <BrowserRouter>
             <div>
+                <Link to="/number-baseball">숫자야구 </Link>
+                <Link to="/rock-paper-scissors">가위바위보 </Link>
+                <Link to="/lotto-generator">로또생성기</Link>
+            </div>
+            <div>
                 <Route path="/number-baseball" component={NumberBaseball}></Route>
-                <Route path="/rock-paper-scissors" component={RockPaperScissorsHooks}></Route>
-                <Route path="/lotto-generator" component={LottoHooks}></Route>
+                <Route path="/rock-paper-scissors" component={RockPaperScissors}></Route>
+                <Route path="/lotto-generator" component={Lotto}></Route>
             </div>
         </BrowserRouter>
     )
